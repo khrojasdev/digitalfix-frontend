8a84dfe71c29b62a8a4ec1f27a89ce818b41acce
Diego Javier López Fuentes <dij.lopez@duocuc.cl>
2026-09-13 19:41:33 -0300

    feat(auth): implementar MsalGuard para proteger rutas del sistema. Closes #159

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,15 +10,16 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { routes } from './app.routes';
 import { msalConfig } from './core/auth/msal.config';
 
-import { MsalModule } from '@azure/msal-angular';
+import { MsalModule, MsalGuard } from '@azure/msal-angular'; // <-- Importa MsalGuard
 import { PublicClientApplication, InteractionType } from '@azure/msal-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(withEventReplay()), // <-- Tu configuración original conservada
     provideHttpClient(),
+    MsalGuard, // <-- Registra el guardián globalmente
     importProvidersFrom(
       MsalModule.forRoot(
         new PublicClientApplication(msalConfig),
