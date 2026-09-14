b485bd6c3c082af9bc1ac374fe5777fff85b3449
Diego Javier López Fuentes <dij.lopez@duocuc.cl>
2026-09-13 22:29:07 -0300

    feat(auth): exponer roles y metodos de validacion en SessionService. Closes #162

--- src/app/core/auth/session.service.ts
+++ src/app/core/auth/session.service.ts
@@ -7,6 +7,7 @@ export interface UserProfile {
   name: string;
   email: string;
   roles: string[];
+  scopes: string[];
 }
 
 @Injectable({
@@ -25,14 +26,29 @@ export class SessionService {
       .subscribe(() => {
         const account = this.msalService.instance.getActiveAccount();
         if (account) {
+          // Entra ID suele enviar los scopes como un string separado por espacios en el claim 'scp'
+          const scpClaim = account.idTokenClaims?.['scp'];
+          const parsedScopes = typeof scpClaim === 'string' ? scpClaim.split(' ') : [];
+
           this.currentUserSubject.next({
             name: account.name || '',
             email: account.username || '',
             roles: (account.idTokenClaims?.['roles'] as string[]) || [],
+            scopes: parsedScopes,
           });
         } else {
           this.currentUserSubject.next(null);
         }
       });
+  }
+
+  public hasRole(role: string): boolean {
+    const user = this.currentUserSubject.value;
+    return user ? user.roles.includes(role) : false;
+  }
+
+  public hasAnyRole(roles: string[]): boolean {
+    const user = this.currentUserSubject.value;
+    return user ? roles.some((r) => user.roles.includes(r)) : false;
   }
 }
